0fc6851adde49ace2c5d85bc6973542d0c6b4695
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-10 15:18:35 +0530

    Added Card UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,18 @@
+import { ReactComponent as IconDice } from "../assets/images/icon-dice.svg"
+import { ReactComponent as PatternDivider } from "../assets/images/pattern-divider-desktop.svg"
 const Card = () => {
-  return (
-    <div className="card">Card</div>
-  )
+    return (
+        <div className="card">
+            <h5>Advice #115</h5>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+                maiores.
+            </p>
+            <PatternDivider className="divider-lg" />
+            <div className="dice">
+                <IconDice />
+            </div>
+        </div>
+    )
 }
 export default Card
