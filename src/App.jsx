2477b6473bf9da3e7f5603ff51dccb67174ec841
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-10 16:17:04 +0530

    Implemented routing using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import "./styles/App.scss"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route, Link } from "react-router-dom"
+import Card from "./components/Card"
+import SearchPage from "./components/SearchPage"
 
 function App() {
     return (
         <div className="App">
             <div className="tab-bar">
-                <div className="tab-item">Home</div>
-                <div className="tab-item">Search</div>
+                <Link to="/">
+                    <div className="tab-item-active">Home</div>
+                </Link>
+                <Link to="/search">
+                    <div className="tab-item">Search</div>
+                </Link>
             </div>
-            <main></main>
+            <main>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={<Card />}
+                    ></Route>
+                    <Route
+                        exact
+                        path="/search"
+                        element={<SearchPage />}
+                    ></Route>
+                </Routes>
+            </main>
         </div>
     )
 }
