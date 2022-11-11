33f8279ea489eb56c17fe806c04065f3f47ba8bf
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-11 11:26:15 +0530

    Divider view based on screen size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react"
 import { ReactComponent as IconDice } from "../assets/images/icon-dice.svg"
-import { ReactComponent as PatternDivider } from "../assets/images/pattern-divider-desktop.svg"
+import { ReactComponent as PatternDividerLg } from "../assets/images/pattern-divider-desktop.svg"
+import { ReactComponent as PatternDividerMd } from "../assets/images/pattern-divider-mobile.svg"
+
 const Card = () => {
+    const [width, setWidth] = useState(window.innerWidth)
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth)
+    }
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowSizeChange)
+        return () => {
+            window.removeEventListener("resize", handleWindowSizeChange)
+        }
+    }, [])
+
+    const isMobile = width <= 768
     return (
         <div className="card">
             <h5>Advice #115</h5>
@@ -8,7 +24,11 @@ const Card = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
                 maiores.
             </p>
-            <PatternDivider className="divider-lg" />
+            {isMobile ? (
+                <PatternDividerMd className="divider-md" />
+            ) : (
+                <PatternDividerLg className="divider-lg" />
+            )}
             <div className="dice">
                 <IconDice />
             </div>
