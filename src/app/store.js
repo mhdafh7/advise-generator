9d9861fc5663357891582a67942b088360349844
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-14 23:04:18 +0530

    removed default state

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { apiSlice } from "../features/api/apiSlice"
 
-const initialQuery = {
-    value: "",
-}
-
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
