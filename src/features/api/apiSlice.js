ac3fc0b6fdb6d3c0233c316e4656aa4eddf7816d
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-14 06:53:26 +0530

    api fetch using RTK query

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const apiSlice = createApi({
+    reducerPath: "apiSlice",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "https://api.adviceslip.com/",
+    }),
+    endpoints: (builder) => ({
+        getAdvice: builder.query({
+            query: () => "advice",
+        }),
+        getSearchAdvice: builder.query({
+            query: (searchItem) => `advice/search/${searchItem}`,
+        })
+    }),
+})
+
+export const { useGetAdviceQuery, useGetSearchAdviceQuery } = apiSlice
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./styles/index.scss"
+import { store } from "./app/store"
+import { Provider } from "react-redux"
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react"
+import { apiSlice } from "./features/api/apiSlice"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <ApiProvider api={apiSlice}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </ApiProvider>
+        </Provider>
     </React.StrictMode>
 )
