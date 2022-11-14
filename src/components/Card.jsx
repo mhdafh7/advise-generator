c22f90a9b4dcefad86197e82807cae41e731e394
mhdafh7 <muhammed.aflah@leegality.com>
2022-11-14 08:03:00 +0530

    Added responsiveness and improved data rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,42 +1,33 @@
-import { useState, useEffect } from "react"
 import { ReactComponent as IconDice } from "../assets/images/icon-dice.svg"
 import { ReactComponent as PatternDividerLg } from "../assets/images/pattern-divider-desktop.svg"
 import { ReactComponent as PatternDividerMd } from "../assets/images/pattern-divider-mobile.svg"
-import axios from "axios"
+import { useState, useEffect } from "react"
 
-const Card = ({ isMobile }) => {
-    const [advice, setAdvice] = useState([])
+const Card = ({ advice, id }) => {
+    const [width, setWidth] = useState(window.innerWidth)
 
-    const baseUrl = "https://api.adviceslip.com/advice"
-
-    const getAdvice = () => {
-        axios
-            .get(baseUrl)
-            .then((res) => {
-                setAdvice(res.data.slip)
-                console.log(res.data.slip)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+    const handleWindowSizeChange = () => {
+        setWidth(window.innerWidth)
     }
     useEffect(() => {
-        getAdvice()
+        window.addEventListener("resize", handleWindowSizeChange)
+        return () => {
+            window.removeEventListener("resize", handleWindowSizeChange)
+        }
     }, [])
+    
+    const isMobile = width <= 768
 
     return (
         <div className="card">
-            <h5>Advice #{advice.id}</h5>
-            <p>{advice.advice}</p>
+            <h5>Advice #{id}</h5>
+            <p>{advice}</p>
             {isMobile ? (
                 <PatternDividerMd className="divider-md" />
             ) : (
                 <PatternDividerLg className="divider-lg" />
             )}
-            <div
-                className="dice"
-                onClick={getAdvice}
-            >
+            <div className="dice">
                 <IconDice />
             </div>
         </div>
